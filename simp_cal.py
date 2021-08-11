8b8b9ffa0219c4d00566c6343b61bab4a4acd3f5
callum thomas <callumthomas086@gmail.com>
2021-08-11 09:32:03 +0100

    workin v0.1

--- simp_cal.py
+++ simp_cal.py
@@ -20,11 +20,10 @@ def button_clear():
 
 # creating the button for addition
 def button_addition():
-    First_number = e.get()
+    first_num = e.get()
     global f_num 
-    f_num = int(First_number)
+    f_num = int(first_num)
     e.delete(0, END)
-
 
 #creating the functions for the equals
 def button_equal ():
@@ -32,16 +31,7 @@ def button_equal ():
     e.delete(0, END)
     e.insert(0, f_num + int(second_number))
 
-def button_divide():
-    return
 
-
-def button_multiply():
-    return
-
-
-def button_subtract():
-    return
 
 # def buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
@@ -54,14 +44,10 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda:button_click(
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda:button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda:button_click(9))
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda:button_click(0))
-button_addition = Button(root, text="+", padx=39, pady=20, command=button_addition())
+button_addition = Button(root, text="+", padx=39, pady=20, command=button_addition)
 button_equal = Button(root, text="=", padx=91, pady=20, command=lambda:button_click())
 button_CE = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
-button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtract())
-button_multiply = Button(root, text="x", padx=39, pady=20, command=button_multiply())
-button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide())
-# put buttons on the screen 
 
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
@@ -80,9 +66,6 @@ button_CE.grid(row=4, column=1, columnspan=2)
 button_equal.grid(row=5,column=1,columnspan=2)
 button_addition.grid(row=5, column=0)
 
-button_subtract.grid (row=6, column=0)
-button_multiply.grid (row=6, column=1)
-button_divide.grid (row=6, column=2)
 
 
 mainloop()
